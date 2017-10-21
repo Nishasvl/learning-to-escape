dd11b6f7027638c9b5e30bdbf6e5eee76721f6d1
linklegend <link_leagend@hotmail.com>
2017-10-22 09:55:31 +1100

    testing fail

--- core/src/mycontroller/MyAIController.java
+++ core/src/mycontroller/MyAIController.java
@@ -27,7 +27,7 @@ public class MyAIController extends CarController{
 	private WorldSpatial.Direction previousState = null; // Keeps track of the previous state
 	
 	// Car Speed to move at
-	private float CAR_SPEED = 2;
+	private float CAR_SPEED = 1;
 	
 	// Offset used to differentiate between 0 and 360 degrees
 	private int EAST_THRESHOLD = 3;
@@ -123,6 +123,7 @@ public class MyAIController extends CarController{
 				else if(getOrientation().equals(WorldSpatial.Direction.EAST)) {
 					applyReverseAcceleration();
 					isGoingBackward = true;
+					isFollowingCoordinate = true;
 					
 				}
 				else{
@@ -204,7 +205,7 @@ public class MyAIController extends CarController{
 				/*if there is no turn ahead, remain original car speed*/
 				if(!CheckTurningAhead(getOrientation(),currentCoordinate,currentView, delta) && getSpeed() < CAR_SPEED){
 					applyForwardAcceleration();
-					CAR_SPEED = 2 ;
+					CAR_SPEED = 1 ;
 				}
 				/*if trap is in ahead*/
 				if(route.size() > 2) {
